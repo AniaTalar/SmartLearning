72d1468a1ebde5f744191d27c58e1657bd028301
Ania <antaludemy@op.pl>
2024-10-27 17:49:22 +0100

    List of posts
    The list of posts doesn't show itself.

--- SmartLearning/SmartLearning/smart_blog/urls.py
+++ SmartLearning/SmartLearning/smart_blog/urls.py
@@ -6,6 +6,6 @@ app_name = 'smart_blog'
 
 urlpatterns = [
     # post views
-    path('', views.post_list, name='post_list'),
+    path('list/', views.post_list, name='post_list'),
     path('<int:id>/', views.post_detail, name='post_detail'),
 ]
